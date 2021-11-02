238f06e3d12deffae923d803296c33ec3b0734a8
TiaSinghania <sonicstormwind@gmail.com>
2021-11-01 17:41:43 -0700

    Started mecanum joystick command and created subsystem

---
+++ src/main/java/frc/robot/commands/JoystickControllerCommand.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.MecanumSubsystem;
+
+/** An example command that uses an example subsystem. */
+public class JoystickControllerCommand extends CommandBase {
+  private final MecanumSubsystem m_subsystem;
+  private final XboxController m_xboxController;
+
+  public JoystickControllerCommand(MecanumSubsystem subsystem, XboxController xboxController) {
+    m_subsystem = subsystem;
+    m_xboxController = xboxController;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      m_subsystem.move(m_xboxController.getX(), m_xboxController.getY(), m_xboxController.get)
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
